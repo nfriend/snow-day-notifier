583e23cabe5a2f5518b6f8839a36bde5ee78ef65
Nathan Friend <nathan@gitlab.com>
2019-11-16 13:27:15 -0400

    Testing passing IFTTT keys

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import { createIftttEvent } from './create-ifttt-event';
 
 module.exports.checkForDelays = async () => {
   const checkSiteResults = await checkSiteForDelay();
+
+  const iftttKeys = JSON.parse(process.env.IFTTT_KEYS);
+
+  console.log('iftttKeys:', iftttKeys);
+
   if (checkSiteResults.newDelayOrCancellationDetected) {
-    await createIftttEvent(checkSiteResults.matchedArticleTitle, [
-      'todo: key here',
-    ]);
+    await createIftttEvent(checkSiteResults.matchedArticleTitle, iftttKeys);
   }
 };
